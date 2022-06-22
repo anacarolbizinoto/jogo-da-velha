5faaccbda7b7ed70b4e8b2e1034d3d1cf3f2101b
Ana Carolina Bizinoto <acbsnutri@gmail.com>
2022-06-22 17:30:20 -0300

    adding winning combination

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const cellElements = document.querySelectorAll("[data-cell]");
 
 let isCircleTurn = false;
 
+ const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+ ]
+
 const placeMark = (cell, classToAdd) => {
     cell.classList.add(classToAdd)
 }
@@ -18,7 +29,12 @@ const handleClick = (e) => {
     cell.classList.add(classToAdd);
 
     // verificar vitória
+
+
     // verificar empate
+
+
+
     // mudar de simbolo
 
     swapTurns();
